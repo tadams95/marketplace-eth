fd1678ec0eab9210f986d99fd0fc438f7eabf281
tadams95 <tyrelleadams28@gmail.com>
2023-10-16 22:28:15 -0700

    get ETH address accounts

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,8 +1,17 @@
 
+import { useEffect, useState } from "react"
 
 export const handler = web3 => () => {
+  const [account, setAccount] = useState(null)
 
-    return {
-        account: web3 ? "Test Account" : "null"
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3.eth.getAccounts()
+      setAccount(accounts[0])
     }
+
+    web3 && getAccount()
+  }, [])
+
+  return { account }
 }
