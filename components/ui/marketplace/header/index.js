aafa6eb40dc36f719ca8b2bd022a24e3ab4fe41d
tadams95 <tyrelleadams28@gmail.com>
2023-10-20 16:21:56 -0700

    map through market page links in breadcrumbs

--- components/ui/marketplace/header/index.js
+++ components/ui/marketplace/header/index.js
@@ -1,6 +1,21 @@
 import Breadcrumbs from "../../common/breadcrumbs";
 import { EthRates, Walletbar } from "../../web3";
 
+const LINKS = [
+  {
+    href: "/marketplace",
+    value: "Buy",
+  },
+  {
+    href: "/marketplace/courses/owned",
+    value: "My Courses",
+  },
+  {
+    href: "/marketplace/courses/manage",
+    value: "Manage Courses",
+  },
+];
+
 export default function Header() {
   return (
     <>
@@ -8,7 +23,7 @@ export default function Header() {
 
       <EthRates />
       <div className="flex flex-row-reverse py-4 px-4 sm:px-6 lg:px-8">
-        <Breadcrumbs />
+        <Breadcrumbs items={LINKS} />
       </div>
     </>
   );
